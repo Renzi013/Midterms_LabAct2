5577a04ff275884d36741ce5b5a8e3f62dce743c
algieili <ilialgie75@gmail.com>
2025-10-14 22:50:19 +0800

    Implement routing and unpopulated shell components

---
+++ src/components/AddTask.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function AddTask() {
+    return (
+    <div>
+        <h2>Add Task View</h2>
+        <p>This will display the task form.</p>
+    </div>
+    );
+}
+
+export default AddTask;
---
+++ src/components/TaskList.js
@@ -0,0 +1,11 @@
+function TaskList() {
+    return(
+<div>
+    <h2>Task List View</h2>
+    <p>This will display the task list</p>
+</div>
+
+    );
+
+}
+export default TaskList
