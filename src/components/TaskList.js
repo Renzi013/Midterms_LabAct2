be0f5e1420ae765a4b86db9aebe3d3a0b7fd0765
John Michael Gigante <johnmichaelgigante25@gmail.com>
2025-10-15 09:01:17 +0800

    Display task list with delete confirmation

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,51 @@
-function TaskList() {
+import React from "react";
+
+function TaskList({tasks, deletTask}) {
+    const confirmDelete = (id) => {
+        if(window.confirm("Are you sure you want to delete this task?")){
+            deletTask(id);
+        }
+    };
+
     return(
 <div>
     <h2>Task List View</h2>
-    <p>This will display the task list</p>
+    <table className="table table-bordered table-striped mt-3">
+        <thead className="table-dark">
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Priority</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            {tasks.lengh > 0 ?(
+                tasks.map((task) => (
+                    <tr key={task.id}>
+                        <td>{task.id}</td>
+                        <td>{task.title}</td>
+                        <td>{task.description}</td>
+                        <td>{task.priority}</td>
+                        <td>
+                            <button className="btn btn-danger btn-sm"
+                            onClick={() => confirmDelete}>
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+                ))
+            ): (
+                <tr>
+                    <td colSpan="5" className="text-center">
+                        No tasks available
+                    </td>
+                </tr>
+            )}
+        </tbody>
+    </table>
 </div>
-
-    );
-
+);
 }
 export default TaskList
